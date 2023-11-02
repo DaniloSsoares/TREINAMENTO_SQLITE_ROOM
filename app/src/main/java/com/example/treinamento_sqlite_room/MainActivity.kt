415ae3d1c03eff951b226bab5cecc579b0555f5b
DaniloSsoares <danilo.ssoares201@gmail.com>
2023-11-01 22:19:11 -0300

    Initial commit

---
+++ app/src/main/java/com/example/treinamento_sqlite_room/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.treinamento_sqlite_room
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.treinamento_sqlite_room.ui.theme.TREINAMENTO_SQLITE_ROOMTheme
+import kotlinx.coroutines.flow.flow
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TREINAMENTO_SQLITE_ROOMTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+
+
+
+
